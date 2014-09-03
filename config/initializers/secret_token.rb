e3e9b4728ebec72534106c533dc21a2c453019ce
andresjordanze <andresjordanze@gmail.com>
2014-09-03 09:01:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gangamall::Application.config.secret_key_base = '5a5b1c2ad8545e6f6e2b330273cfb09817144acf9c4719c8e3326652acd152007fcd86bdfb5d5c12ff53013f8d92d30975ff8f9740c228c23982d48ad444234a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gangamall::Application.config.session_store :cookie_store, key: '_gangamall_session'
